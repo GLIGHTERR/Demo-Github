b909fca726f8b9eb2385408c6affc284fc672c39
GLIGHTERR <tuannghianguyen1509@gmail.com>
2021-06-27 23:49:20 +0700

    First commit

---
+++ src/demo/DemoOnly.java
@@ -0,0 +1,51 @@
+package demo;
+
+import java.util.*;
+
+public class DemoOnly {
+	static Scanner sc = new Scanner(System.in);
+	static int [] a = new int[100];
+
+	public static void main(String[] args) {
+		int n = Integer.parseInt(sc.nextLine());
+		node p = null;
+		for (int i = 0; i < n; i++) {
+			int x = Integer.parseInt(sc.nextLine());
+			p = insert(p, x);
+		}
+		
+		System.out.println(countLevel(p));
+	}
+	
+	private static int countLevel(node p) {
+		if (p == null) {
+			return -1;
+		}
+		
+		return 1 + Math.max(countLevel(p.left), countLevel(p.right));
+	}
+
+	private static node insert(node p, int x) {
+		if (p == null) {
+			node q = new node();
+			q.info = x;
+			q.left = null;
+			q.right = null;
+			return q;
+		} else {
+			if (x < p.info) {
+				p.left = insert(p.left, x);
+			} else {
+				p.right = insert(p.right, x);
+			}
+		}
+		
+		return p;
+	}
+}
+
+class node {
+	int info;
+	node left;
+	node right;
+}
---
+++ src/demo/DemoOnly2.java
@@ -0,0 +1,13 @@
+package demo;
+
+import java.util.Scanner;
+
+public class DemoOnly2 {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
